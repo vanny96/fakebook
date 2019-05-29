03148201fd95ff4bd3b1af6e73d34c48ee097303
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-05-29 18:15:10 +0200

    Add friendships

---
+++ app/models/friendship.rb
@@ -0,0 +1,19 @@
+class Friendship < ApplicationRecord
+  after_create :create_inverse_friendship
+  after_destroy :destroy_inverse_friendship
+
+  belongs_to :user
+  belongs_to :friend, class_name: 'User'
+
+  validates :user, uniqueness: {scope: :friend}
+
+  def create_inverse_friendship
+    Friendship.create(user_id: self.friend_id, friend_id: self.user_id)
+  end
+
+  def destroy_inverse_friendship
+    if inverse_friendship = Friendship.where("user_id = ? OR friend_id = ?", self.friend_id, self.user_id).first
+      inverse_friendship.destroy
+    end
+  end
+end
