95be6df2095842f38349df82b562dd9f156707c3
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-05-29 15:15:46 +0200

    Change app controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
     root to: "devise/sessions#new"
   end
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  resources :users, except: [:new, :create, :edit, :update] 
 end
