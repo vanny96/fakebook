c0d91ee6319820678a4c0f25c78fefae53cfe361
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-05-29 14:29:06 +0200

    Add callback controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_scope :user do
     root to: "devise/sessions#new"
   end
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
