ceb743508b83542a0493f97e4dccd038c05b3748
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-06-01 13:01:36 +0200

    Only shows authenticated users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+  before_action :user_authenticated?, only: :show
+
   def index
-    @users = User.all
+    @users = User.all.filter{|user| user.active_for_authentication?}
   end
 
   def show
-    @user = User.find(params[:id])
+    
     
     @requests = @user.friendship_request_received.includes(:user)
     @user_posts = @user.posts
@@ -19,5 +21,15 @@ class UsersController < ApplicationController
 
   def show_friends
     @friends = User.find(params[:id]).friends.page(1).per(20)
+  end
+
+  private
+
+  def user_authenticated?
+    @user = User.find(params[:id])
+    unless @user.active_for_authentication?
+      redirect_to users_path 
+      flash[:error] = "User not yet authenticated"
+    end
   end
 end
