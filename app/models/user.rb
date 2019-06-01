ae72979e4b59ea1e7e18f80aa74e30a8ce57324b
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-06-01 12:20:06 +0200

    Add no need to confirmatio email if using omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,7 @@ class User < ApplicationRecord
   # Asks the OAuth provider for email, password and full_name
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.skip_confirmation!
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.full_name = auth.info.name   # assuming the user model has a name
@@ -48,6 +49,7 @@ class User < ApplicationRecord
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.skip_confirmation!
         user.email = data["email"] if user.email.blank?
       end
     end
